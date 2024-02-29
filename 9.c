5dd6279e5b96acfa45a9f533542fa6cdfa25cf84
Brian Martinez <132228607+brianprogh1@users.noreply.github.com>
2024-03-01 00:41:03 +0530

    Update 9.c

--- 9.c
+++ 9.c
@@ -36,24 +36,67 @@ NODE insertrear(NODE h, int cf, int px, int py, int pz)
     return h;
 }
 
-
-void readpoly(NODE h)
+NODE readpoly(NODE h)
 {
     int cf, px, py, pz, ch;
+
     do
     {
-        printf("enter co-eff, px, py, pz:");
+        printf("Enter coefficient, px, py, pz:");
         scanf("%d%d%d%d", &cf, &px, &py, &pz);
         h = insertrear(h, cf, px, py, pz);
-        printf("\n1 to continue 0 to exit:");
+        printf("\n1 to continue, 0 to exit:");
         scanf("%d", &ch);
     } while (ch != 0);
+
+    return h;
 }
 
-void addpoly(NODE h1, NODE h2, NODE h3)
+void evalpoly(NODE h1)
+{
+    int x, y, z;
+    float result = 0.0;
+    NODE temp = h1->link;
+
+    printf("\nEnter the value of x, y, z:");
+    scanf("%d%d%d", &x, &y, &z);
+
+    while (temp != h1)
+    {
+        result = result + temp->cf * pow(x, temp->px) * pow(y, temp->py) * pow(z, temp->pz);
+        temp = temp->link;
+    }
+
+    printf("\nThe result = %f\n", result);
+}
+
+void display(NODE h1)
+{
+    NODE temp;
+
+    if (h1->link == h1)
+    {
+        printf("\nPolynomial is empty\n");
+        return;
+    }
+
+    temp = h1->link;
+    while (temp != h1)
+    {
+        if (temp->cf > 0)
+            printf("+%dx^%dy^%dz^%d", temp->cf, temp->px, temp->py, temp->pz);
+        else
+            printf("%dx^%dy^%dz^%d", temp->cf, temp->px, temp->py, temp->pz);
+
+        temp = temp->link;
+    }
+}
+
+NODE addpoly(NODE h1, NODE h2, NODE h3)
 {
     NODE p1, p2;
     int cf1, px1, py1, pz1, cf2, px2, py2, pz2, cf;
+
     p1 = h1->link;
 
     while (p1 != h1)
@@ -62,6 +105,7 @@ void addpoly(NODE h1, NODE h2, NODE h3)
         px1 = p1->px;
         py1 = p1->py;
         pz1 = p1->pz;
+
         p2 = h2->link;
 
         while (p2 != h2)
@@ -104,54 +148,15 @@ void addpoly(NODE h1, NODE h2, NODE h3)
 
         p2 = p2->link;
     }
+
+    return h3;
 }
 
-
-void evalpoly(NODE h1)
-{
-    int x, y, z;
-    float result = 0.0;
-    NODE temp = h1->link;
-    
-    printf("\nenter the value of x, y, z:");
-    scanf("%d%d%d", &x, &y, &z);
-
-    while (temp != h1)
-    {
-        result += temp->cf * pow(x, temp->px) * pow(y, temp->py) * pow(z, temp->pz);
-        temp = temp->link;
-    }
-
-    printf("\nthe result = %f\n", result);
-}
-
-void display(NODE h1)
-{
-    NODE temp;
-    if (h1->link == h1)
-    {
-        printf("\npolynomial is empty");
-        return;
-    }
-
-    temp = h1->link;
-    while (temp != h1)
-    {
-        if (temp->cf > 0)
-            printf("+%dx%dy%dz%d", temp->cf, temp->px, temp->py, temp->pz);
-        else
-            printf("%dx%dy%dz%d", temp->cf, temp->px, temp->py, temp->pz);
-
-        temp = temp->link;
-    }
-}
-
-
-
-int main()
+void main()
 {
     int ch;
     NODE h1, h2, h3;
+
     h1 = (NODE)malloc(sizeof(struct poly));
     h2 = (NODE)malloc(sizeof(struct poly));
     h3 = (NODE)malloc(sizeof(struct poly));
@@ -162,44 +167,39 @@ int main()
 
     while (1)
     {
-        printf("\n1. evaluate a polynomial\n2. add polynomial\n3. exit\n");
-        printf("enter choice:");
+        printf("\n1. Evaluate a polynomial\n2. Add polynomials\n3. Exit\n");
+        printf("Enter choice:");
         scanf("%d", &ch);
 
         switch (ch)
         {
-            case 1:
-                printf("enter polynomial:");
-                readpoly(h1);
-                evalpoly(h1);
-                display(h1);
-                h1->link = h1;
-                break;
+        case 1:
+            printf("Enter polynomial:");
+            h1 = readpoly(h1);
+            evalpoly(h1);
+            display(h1);
+            h1->link = h1;
+            break;
 
-            case 2:
-                printf("enter 1st polynomial:");
-                readpoly(h1);
-                printf("enter 2nd polynomial:");
-                readpoly(h2);
-                addpoly(h1, h2, h3);
+        case 2:
+            printf("Enter 1st polynomial:");
+            h1 = readpoly(h1);
+            printf("Enter 2nd polynomial:");
+            h2 = readpoly(h2);
+            h3 = addpoly(h1, h2, h3);
+            printf("\n1st polynomial is\n");
+            display(h1);
+            printf("\n");
+            printf("\n2nd polynomial is\n");
+            display(h2);
+            printf("\n");
+            printf("\nResultant polynomial is \n");
+            display(h3);
+            printf("\n");
+            break;
 
-                printf("\n1st polynomial is\n");
-                display(h1);
-                printf("\n");
-
-                printf("\n2nd polynomial is\n");
-                display(h2);
-                printf("\n");
-
-                printf("\nresultant polynomial is \n");
-                display(h3);
-                printf("\n");
-                break;
-
-            case 3:
-                exit(0);
+        case 3:
+            exit(0);
         }
     }
-
-    return 0;
 }

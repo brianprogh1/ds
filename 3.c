5d1c134b59eee0002ef1a72ce50cd69cf20df3db
Brian Martinez <132228607+brianprogh1@users.noreply.github.com>
2024-03-01 00:25:32 +0530

    Update 3.c

--- 3.c
+++ 3.c
@@ -1,93 +1,103 @@
-/* Stack Operations */
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 #define maxsize 4
 
-void push(int s[], int *ptop, int ele)
-{
-    if (*ptop == (maxsize - 1))
-        printf("\n\nstack is overflow");
-    else
-        s[++(*ptop)] = ele;
+int push(int s[], int *top) {
+    int ele;
+    if (*top == (maxsize - 1)) {
+        printf("\n\nStack is overflow");
+        return -1;  // Indicate failure to push
+    } else {
+        printf("\nEnter an element to be pushed: ");
+        scanf("%d", &ele);
+        s[++(*top)] = ele;
+        return 0;  // Indicate successful push
+    }
 }
 
-int pop(int s[], int *ptop)
-{
-    return s[(*ptop)--];
+int pop(int s[], int *top) {
+    int ele;
+    ele = s[(*top)--];
+    return ele;
 }
 
-void palindrome(int s[], int top)
-{
-    int flag = 1, i, j;
-    j = top;
-    for (i = 0; i < top / 2; i++, j--)
-        if (s[i] != s[j])
-        {
+void palindrome(int v[], int top) {
+    int flag = 0, i;
+    for (i = 0; i < (top + 1); i++) {
+        if (v[i] == pop(v, &top))
+            flag = 1;
+        else {
             flag = 0;
             break;
         }
-    if (flag == 1)
-        printf("stack contents are palindrome");
+    }
+    if (flag)
+        printf("Stack contents are palindrome\n");
     else
-        printf("stack contents are not palindrome");
+        printf("Stack contents are not palindrome\n");
 }
 
-void display(int s[], int top)
-{
+void display(int s[], int top) {
     int i;
-    if (top == -1)
-    {
-        printf("\n stack is empty");
+    if (top == -1) {
+        printf("\nStack is empty\n");
         return;
-    }
-    else
-    {
-        printf("\n the stack contents are");
-        for (i = top; i >= 0; i--)
+    } else {
+        printf("\nThe stack contents are");
+        for (i = top; i >= 0; i--) {
             printf("\n[%d]", s[i]);
+        }
+        printf("\n");
     }
 }
 
-void main()
-{
-    int s[maxsize], item, ch, top = -1;
-    while (1)
-    {
-        printf("\n----------->MAIN MENU<	\n");
-        printf("\n1---------->PUSH into the stack<	");
-        printf("\n2---------->POP from the stack<	");
-        printf("\n3---------->PALINDROME check using stack<	");
-        printf("\n4----------> DISPLAY<	");
-        printf("\n5---------->EXIT<	");
-        printf("\n enter your choice:");
+int main() {
+    int s[maxsize], ele;
+    int ch, top = -1;
+
+    while (1) {
+        printf("\n----------->MAIN MENU<\n");
+        printf("1---------->PUSH into the stack\n");
+        printf("2---------->POP from the stack\n");
+        printf("3---------->PALINDROME check using stack\n");
+        printf("4---------->DISPLAY\n");
+        printf("5---------->EXIT\n");
+        printf("Enter your choice: ");
         scanf("%d", &ch);
-        switch (ch)
-        {
-        case 1:
-            printf("\n Enter the element to be inserted");
-            scanf("%d", &item);
-            push(s, &top, item);
-            break;
-        case 2:
-            if (top == -1)
-                printf("\n stack under flow");
-            else
-            {
-                item = pop(s, &top);
-                printf("\n popped element is %d", item);
-            }
-            break;
-        case 3:
-            palindrome(s, top);
-            break;
-        case 4:
-            display(s, top);
-            break;
-        case 5:
-            exit(0);
-            break;
-        default:
-            printf("\n enter a valid choice");
+
+        switch (ch) {
+            case 1:
+                if (push(s, &top) == 0)
+                    display(s, top);
+                break;
+
+            case 2:
+                if (top == -1) {
+                    printf("\nStack underflow\n");
+                } else {
+                    ele = pop(s, &top);
+                    printf("\nPopped element is %d\n", ele);
+                }
+                break;
+
+            case 3:
+                palindrome(s, top);
+                break;
+
+            case 4:
+                display(s, top);
+                break;
+
+            case 5:
+                exit(0);
+                break;
+
+            default:
+                printf("\nEnter a valid choice\n");
+                break;
         }
     }
+
+    return 0;
 }
